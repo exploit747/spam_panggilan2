067d4442e502daeab8422ed049ce68f9f73e353a
B374K <44334553+exploit747@users.noreply.github.com>
2019-04-25 13:47:34 +0700

    Update SpamCall.php

--- SpamCall.php
+++ SpamCall.php
@@ -5,7 +5,7 @@
 |      Update    = 07-January-2019             |
 |      TooL Spam = Telpon    		       |
 |      GitHub    = -			       |
-|      Youtube   =                             |
+|      Youtube   =  tes                        |
 |      Info      = Gunakan Dengan Bijak Yee ^_^|
 ------------------------------------------------
 
